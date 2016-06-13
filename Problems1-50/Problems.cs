9c50d5d7dc339d138bf7d5cd5c4a77eaae226c61
DavidNemeth <david.nemeth90@gmail.com>
2016-06-13 10:10:40 +0200

    9 fix

--- Problems1-50/Problems.cs
+++ Problems1-50/Problems.cs
@@ -200,23 +200,21 @@ namespace Problems1_50
             //A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a2 + b2 = c2
             //There exists exactly one Pythagorean triplet for which a + b + c = 1000.
             //Find the product abc.            
-            List<int> triplets = new List<int>(3) { 0, 0, 0 };
-            for (int a = 0; a < 500; a++)
+            List<int> triplets = new List<int>(3);
+            int num = 1000;
+            int a, b, c;
+            for (a = 0; a < num / 3; a++)
             {
-                for (int b = 0; b < 500; b++)
+                for (b = 0; b < num / 2; b++)
                 {
-                    for (int c = 0; c < 500; c++)
+                    c = num - a - b;
+                    if (a * a + b * b == c * c)
                     {
-                        if (a < b && b < c && a * a + b * b == c * c)
-                        {
-                            if (a + b + c == 1000)
-                            {
-                                triplets[0] = a;
-                                triplets[1] = b;
-                                triplets[2] = c;
-                            }
-                        }
+                        triplets.Add(a);
+                        triplets.Add(b);
+                        triplets.Add(c);
                     }
+
                 }
             }
             Console.WriteLine("a: {0}", triplets[0]);
